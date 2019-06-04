abb5c6255b84715b702cc487ea73123323503a17
aelbore <aelbore.sg@gmail.com>
2019-06-04 15:35:28 +0800

    refactor code, use aria-test lib

--- karma.conf.js
+++ karma.conf.js
@@ -1,79 +1,23 @@
-const alias = require('rollup-plugin-strict-alias')
+const { createKarmaConfig, karmaPlugins } = require('aria-test')
+const { alias } = require('aria-build')
+
 const vue = require('rollup-plugin-vue')
-const istanbul = require('rollup-plugin-istanbul')
 
-const { nodeResolve, commonjs, typescript2, createTSConfig } = require('aria-build')
-
-module.exports = function(config) {
-  config.set({
-    frameworks: ['mocha', 'chai'],
-
-    files: [
-      'tests/**/*.spec.js'
-    ],
-
-    preprocessors: {
-      "tests/**/*.spec.js": ["rollup"]
-    },
-
+module.exports = createKarmaConfig({ 
+  frameworks: ['mocha', 'chai'],
+  files: [
+    'tests/**/*.spec.js'
+  ],
+  rollup: {
+    custom: true,
     plugins: [
-      'karma-mocha',
-      'karma-chai',
-      'karma-mocha-reporter',
-      'karma-chrome-launcher',
-      'karma-coverage-istanbul-reporter',
-      'karma-rollup-preprocessor'
-    ],
-
-    rollupPreprocessor: {
-      plugins: [
-        typescript2(createTSConfig()),
-        alias({
-          vue: 'node_modules/vue/dist/vue.js'
-        }),
-        nodeResolve(),
-        vue(),
-        istanbul({ 
-          exclude: [
-            'tests/**/*.spec.js', 
-            "node_modules/**/*"
-          ] 
-        }),
-        commonjs()
-      ],
-      output: {
-        format: 'es',      
-        sourcemap: false
-      }
-    },
-
-    reporters: ['mocha', 'coverage-istanbul'],
-
-    coverageIstanbulReporter: {
-      reports: ['lcov', 'text-summary'],
-      dir: './coverage',
-      combineBrowserReports: true,
-      skipFilesWithNoCoverage: true,
-      thresholds: {
-        global: {
-          statements: 70,
-          branches: 70,
-          functions: 70,
-          lines: 70,
-        }
-      }
-    },
-
-    browsers: ['ChromeHeadlessNoSandbox'],
-
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
-
-    singleRun: true,
-    concurrency: Infinity
-  })
-}
+      alias({
+        vue: 'node_modules/vue/dist/vue.js'
+      }),
+      vue()
+    ]
+  },
+  preprocessors: {
+    'tests/**/*.spec.js': [ 'rollupNode' ]
+  }
+})
